e2767092b0d99a097e5ad00bf268fa085301d9ae
itarun09 <45558826+itarun09@users.noreply.github.com>
2018-12-03 16:30:40 +0530

    Add files via upload
    Object Compare

---
+++ Compare.BL/Compare.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare.BL
+{
+    public class Compare
+    {
+       public static bool CompareObject<T>(T Object1, T object2)
+        {
+            Type type = typeof(T);
+            if (Object1 == null || object2 == null)
+                return false;
+
+            foreach (System.Reflection.PropertyInfo property in type.GetProperties())
+            {
+                if (property.Name != "ExtensionData")
+                {
+                    string Object1Value = string.Empty;
+                    string Object2Value = string.Empty;
+                    if (type.GetProperty(property.Name).GetValue(Object1, null) != null)
+                        Object1Value = type.GetProperty(property.Name).GetValue(Object1, null).ToString();
+                    if (type.GetProperty(property.Name).GetValue(object2, null) != null)
+                        Object2Value = type.GetProperty(property.Name).GetValue(object2, null).ToString();
+                    if (Object1Value.Trim() != Object2Value.Trim())
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Compare.Object/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Compare.BO;
+using Compare.BL;
+
+
+namespace Compare.Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Student1 = new Student() { Name = "Jack", Age = 15, StudentId = 100 };
+            Student Student2 = new Student() { Name = "Smith", Age = 15, StudentId = 101 };
+            Student Student3 = new Student() { Name = "Smith", Age = 15, StudentId = 101 };
+            Student Student4 = new Student() { Name = "Smit", Age = 15, StudentId = 102 };
+            Student Student5 = new Student() { Name = null, Age = 15, StudentId = 100 };
+                        
+            if (Compare.BL.Compare.CompareObject<Student>(Student1, Student2))
+                Console.WriteLine("Both objects are equal");
+            else
+                Console.WriteLine("Both objects are not equal");
+            if (Compare.BL.Compare.CompareObject< Student>(Student3, Student2))
+                Console.WriteLine("Both objects are equal");
+            else
+                Console.WriteLine("Both objects are not equal");
+            if (Compare.BL.Compare.CompareObject< Student>(Student3, Student4))
+                Console.WriteLine("Both objects are equal");
+            else
+                Console.WriteLine("Both objects are not equal");
+            if (Compare.BL.Compare.CompareObject(Student1, Student5))
+                Console.WriteLine("Both objects are equal");
+            else
+                Console.WriteLine("Both objects are not equal");
+            Console.Read();
+        }
+    }
+    }
+
